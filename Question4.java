5981d2193229d3338ce3f2652d77fc339c7b6fbc
nicolelwn <150001048+nicolelwn@users.noreply.github.com>
2024-04-15 14:30:45 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -18,6 +18,10 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
+    int num = in.NextInt
+    while(i-- <= int) {
+      System.out.println(int);
+    }
     
   }
 }
