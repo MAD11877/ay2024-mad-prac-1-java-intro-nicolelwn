adaacb1ab3de2030718ee10317d1f9812d841359
nicolelwn <150001048+nicolelwn@users.noreply.github.com>
2024-04-15 14:45:16 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -20,7 +20,7 @@ public class Question4
     Scanner in = new Scanner(System.in);
     int num = in.nextInt();
     for (i = num; i < (num + 1); i--) {
-      System.out.print(*);
+      System.out.print("*");
     }
     
   }
