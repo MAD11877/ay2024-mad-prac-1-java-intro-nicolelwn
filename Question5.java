23de20ddbd207c2b837cacd5b3b71ef66252aeff
nicolelwn <150001048+nicolelwn@users.noreply.github.com>
2024-04-16 15:11:07 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,34 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    int numOfIntegers = in.nextInt();
+
+    ArrayList<Integer> nums = new ArrayList<>();
+
+    // Loop to get input integers
+    for (int i = 0; i < numOfIntegers; i++) {
+        int num = in.nextInt();
+        nums.add(num);
+    }
+
+    int maxCount = 0;
+    int mode = Integer.MIN_VALUE;
+
+    // Loop to find mode
+    for (int i = 0; i < nums.size(); i++) {
+        int count = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums.get(j) == nums.get(i)) {
+                count++;
+            }
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            mode = nums.get(i);
+        }
+    }
+    
+    System.out.println(mode);
     
   }
 }
